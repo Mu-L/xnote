0b976ba5b3fa9900c44b8241b79786dc48093c07
xupingmao <xupingmao@gmail.com>
2025-11-08 18:47:02 +0800

    ui: improve list-item

--- xnote/plugin/list.py
+++ xnote/plugin/list.py
@@ -15,38 +15,48 @@ class ListViewItem(BaseComponent):
     # 默认链接在外部
     is_link_outside = True
 
-    _code = xtemplate.compile_template("""
-{% if item.is_link_outside %}
-    <a class="list-item {{item.css_class}}" href="{{ item.href }}">
-{% else %}
-    <div class="list-item {{item.css_class}}">
-{% end %}
-
-{% if item.icon_class %}
-    <i class="{{item.icon_class}}"></i>
-{% end %}
-{% if item.is_link_outside %}
-    <span>{{ item.text }}</span>
-{% else %}
-    <a href="{{item.href}}">{{ item.text }}</a>
-{% end %}
-{% for tag in item.tags %} {% render tag %} {% end %}
-<div class="float-right">
-    <span class="book-size-span">{{ item.badge_info }}</span>
-    {% if item.action_btn %}
-        {% render item.action_btn %}
-    {% end %}
-    {% if item.show_chevron_right %}
-        <i class="fa fa-chevron-right"></i>
-    {% end %}
-</div>
-
-{% if item.is_link_outside %}
+    _outside_html = """
+<div class="list-item no-padding">
+    <a class="list-item-link {{item.css_class}}" href="{{ item.href }}">
+        {% if item.icon_class %}
+            <i class="{{item.icon_class}}"></i>
+        {% end %}
+        <span>{{ item.text }}</span>
+        {% for tag in item.tags %} {% render tag %} {% end %}
+        <div class="float-right">
+            <span class="book-size-span">{{ item.badge_info }}</span>
+            {% if item.action_btn %}
+                {% render item.action_btn %}
+            {% end %}
+            {% if item.show_chevron_right %}
+                <i class="fa fa-chevron-right"></i>
+            {% end %}
+        </div>
     </a>
-{% else %}
+</div>
+"""
+
+    _inside_html = """
+<div class="list-item {{item.css_class}}">
+    {% if item.icon_class %}
+        <i class="{{item.icon_class}}"></i>
+    {% end %}
+    <a href="{{item.href}}">{{ item.text }}</a>
+    {% for tag in item.tags %} {% render tag %} {% end %}
+    <div class="float-right">
+        <span class="book-size-span">{{ item.badge_info }}</span>
+        {% if item.action_btn %}
+            {% render item.action_btn %}
+        {% end %}
+        {% if item.show_chevron_right %}
+            <i class="fa fa-chevron-right"></i>
+        {% end %}
     </div>
-{% end %}
-""")
+</div>
+"""
+
+    _outside_code = xtemplate.compile_template(_outside_html)
+    _intside_code = xtemplate.compile_template(_inside_html)
 
     def __init__(self, text="", href="", icon_class="", badge_info="", show_chevron_right = False, css_class="") -> None:
         self.text = text
@@ -61,7 +71,10 @@ class ListViewItem(BaseComponent):
             self.is_link_outside = False
 
     def render(self):
-        return self._code.generate(item = self)
+        if self.is_link_outside:
+            return self._outside_code.generate(item = self)
+        else:
+            return self._intside_code.generate(item = self)
 
 class _ListViewOption:
 
